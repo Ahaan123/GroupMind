3b44ec8a287be08b301f51388358b4036d8c1621
Lakshay Bansal <lakshaybansal2019@gmail.com>
2025-02-15 13:08:32 +0000

    backend

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from reddit_scaper import *
+from summarizer import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        subreddit_name = request.form['subreddit']
+        posts = get_top_posts(subreddit_name)
+        all_text = " ".join([post['title'] + " " + post['selftext'] + " " + " ".join(post['comments']) for post in posts])
+        summary = generate_summary(all_text)
+        return render_template('index.html', summary=summary, subreddit=subreddit_name)
     return render_template('index.html')
 
 if __name__ == '__main__':
